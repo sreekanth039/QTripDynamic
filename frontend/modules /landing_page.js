62b39758c72e0a22ed220fde0b9300b5650f9206
sreekanth mekala <166038635+sreekanth039@users.noreply.github.com>
2025-08-14 19:00:06 +0100

    Create landing_page.js

---
+++ frontend/modules /landing_page.js
@@ -0,0 +1,54 @@
+import config from "../conf/index.js";
+
+async function init() {
+  //Fetches list of all cities along with their images and description
+  let cities = await fetchCities();
+
+  //Updates the DOM with the cities
+  cities.forEach((key) => {
+    addCityToDOM(key.id, key.city, key.description, key.image);
+  });
+}
+
+//Implementation of fetch call
+async function fetchCities() {
+  // TODO: MODULE_CITIES
+  // 1. Fetch cities using the Backend API and return the data
+  try{
+    let fetchApi = await fetch("http://3.109.123.167:8082/cities");
+    let finalData = await fetchApi.json();
+  
+    return finalData;
+
+  }catch(err){
+   return null;
+  }
+
+}
+
+//Implementation of DOM manipulation to add cities
+function addCityToDOM(id, city, description, image) {
+  // TODO: MODULE_CITIES
+  // 1. Populate the City details and insert those details into the DOM
+
+  let container = document.getElementById("data");
+  
+  let div = document.createElement("div");
+  div.className = "col-lg-3 col-6 mt-3";
+
+  div.innerHTML = `
+  <a href="pages/adventures/?city=${id}" id="${id}">
+  <div class= "tile ">
+  <img src="${image}" alt="${city}" class="img-fluid" />
+  <div class = "tile-text text-center">
+  <h5> ${city}</h5>
+  <h5> ${description}</h5>
+  </div>
+  </div>
+  </a>
+  `
+  container.append(div);
+ 
+}
+
+export { init, fetchCities, addCityToDOM };
